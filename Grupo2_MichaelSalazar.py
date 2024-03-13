bf82a968a0d71c85c938fbc010c3a680f535764a
Michael10Crack <michaeldavidsalazar46@gmail.com>
2024-03-13 09:28:40 -0500

    Creación esquemática de clases y el menú

---
+++ Grupo2_MichaelSalazar.py
@@ -0,0 +1,43 @@
+def ValNumInt(x):
+    while True:
+        try:
+            x = int(x)
+            return x
+        except ValueError:
+            return ValNumInt(input('Debe ingresar un dato numérico: '))
+        
+class ImplanteMedico:
+    def __init__(self):
+        pass
+
+class ProtesisCadera(ImplanteMedico):
+    def __init__(self):
+        pass
+
+class MarcapasosCoronoario(ImplanteMedico):
+    def __init__(self):
+        pass
+
+class StentCoronario(ImplanteMedico):
+    def __init__(self):
+        pass
+
+class ImplantesDentales(ImplanteMedico):
+    def __init__(self):
+        pass
+    
+class ProtesisRodilla(ImplanteMedico):
+    def __init__(self):
+        pass
+
+def main():
+    while True:
+        menu = ValNumInt('''
+                         Bienvenido al sistema.
+                         Seleccione la operación que desea realizar
+                         1. Agregar nuevo implante
+                         2. Eliminar implante
+                         3. Editar información 
+                         4. Visualizar inventario completo.
+                         -> ''')
+        

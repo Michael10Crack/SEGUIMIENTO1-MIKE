b8a913cfa2d5b1fdc2cd7417bd84bb48795bd5ff
Michael10Crack <michaeldavidsalazar46@gmail.com>
2024-03-13 21:59:48 -0500

    implementación y adaptación del seguimiento de la vida útil de los implantes

--- Grupo2_MichaelSalazar.py
+++ Grupo2_MichaelSalazar.py
@@ -101,11 +101,13 @@ class Paciente:
 
 
 class MarcapasosCoronario(ImplanteMedico):
-    def __init__(self, tipo , funcion , electrodos, conexion, frecuencia):
+    def __init__(self, tipo , funcion , electrodos, conexion, frecuencia, fecha_revision, fecha_mantenimiento):
         super().__init__(tipo, funcion)
         self.__electrodos = electrodos
         self.__conexion = conexion
         self.__frecuencia = frecuencia
+        self.__fecha_revision = fecha_revision
+        self.__fecha_mantenimiento = fecha_mantenimiento
     def getElectrodos(self):
         return self.__electrodos
     def getConexion(self):
@@ -119,14 +121,16 @@ class MarcapasosCoronario(ImplanteMedico):
     def setFrecuencia(self, f):
         self.__frecuencia = f
     def __str__(self):
-        return f"Tipo: {self._ImplanteMedico__tipo}, Función: {self._ImplanteMedico__funcion}, Electrodos: {self.__electrodos}, Conexión: {self.__conexion}, Frecuencia: {self.__frecuencia}"
+        return f"Tipo: {self._ImplanteMedico__tipo}, Función: {self._ImplanteMedico__funcion}, Electrodos: {self.__electrodos}, Conexión: {self.__conexion}, Frecuencia: {self.__frecuencia}, Fecha de revisión: {self.__fecha_revision}, Fecha de mantenimiento: {self.__fecha_mantenimiento}"
     
 class StentCoronario(ImplanteMedico):
-    def __init__(self, tipo , funcion, longitud, diametro, material):
+    def __init__(self, tipo , funcion, longitud, diametro, material, fecha_revision, fecha_mantenimiento):
         super().__init__(tipo,funcion)
         self.__longitud = longitud
         self.__diametro = diametro
         self.__material = material
+        self.__fecha_revision = fecha_revision
+        self.__fecha_mantenimiento = fecha_mantenimiento
     def getLongitud(self):
         return self.__longitud
     def getDiametro(self):
@@ -140,16 +144,18 @@ class StentCoronario(ImplanteMedico):
     def setMaterial(self,m):
         self.__material = m
     def __str__(self):
-        return f"Tipo: {self._ImplanteMedico__tipo}, Función: {self._ImplanteMedico__funcion}, Longitud: {self.__longitud}, Diámetro: {self.__diametro}, Material: {self.__material}"
+        return f"Tipo: {self._ImplanteMedico__tipo}, Función: {self._ImplanteMedico__funcion}, Longitud: {self.__longitud}, Diámetro: {self.__diametro}, Material: {self.__material}, Fecha de revisión: {self.__fecha_revision}, Fecha de mantenimiento: {self.__fecha_mantenimiento}"
     
 
 
 class ImplantesDentales(ImplanteMedico):
-    def __init__(self, tipo , funcion, forma , sistema_fijacion, material):
+    def __init__(self, tipo , funcion, forma , sistema_fijacion, material, fecha_revision, fecha_mantenimiento):
         super().__init__(tipo, funcion)
         self.__forma = forma 
         self.__sistema_fijacion = sistema_fijacion
         self.__material = material
+        self.__fecha_revision = fecha_revision
+        self.__fecha_mantenimiento = fecha_mantenimiento
     def getForma(self):
         return self.__forma
     def getSisFijacion(self):
@@ -163,16 +169,18 @@ class ImplantesDentales(ImplanteMedico):
     def setMaterial(self,m):
         self.__material = m
     def __str__(self):
-        return f"Tipo: {self._ImplanteMedico__tipo}, Función: {self._ImplanteMedico__funcion}, Forma: {self.__forma}, Sistema de Fijación: {self.__sistema_fijacion}, Material: {self.__material}"
+        return f"Tipo: {self._ImplanteMedico__tipo}, Función: {self._ImplanteMedico__funcion}, Forma: {self.__forma}, Sistema de Fijación: {self.__sistema_fijacion}, Material: {self.__material}, Fecha de revisión: {self.__fecha_revision}, Fecha de mantenimiento: {self.__fecha_mantenimiento}"
 
         
 
 class ImplanteRodilla(ImplanteMedico):
-    def __init__(self, tipo , funcion, material , tipo_fijacion, tamaño):
+    def __init__(self, tipo , funcion, material , tipo_fijacion, tamaño, fecha_revision, fecha_mantenimiento):
         super().__init__(tipo, funcion)
         self.__material = material
         self.__tipo_fijacion = tipo_fijacion
         self.__tamaño = tamaño
+        self.__fecha_revision = fecha_revision
+        self.__fecha_mantenimiento = fecha_mantenimiento
     def getMaterial(self):
         return self.__material
     def getTipoFijacion(self):
@@ -186,16 +194,18 @@ class ImplanteRodilla(ImplanteMedico):
     def setTamaño(self, t):
         self.__tamaño = t
     def __str__(self):
-        return f"Tipo: {self._ImplanteMedico__tipo}, Función: {self._ImplanteMedico__funcion}, Material: {self.__material}, Tipo de fijación: {self.__tipo_fijacion}, Tamaño: {self.__tamaño}"
+        return f"Tipo: {self._ImplanteMedico__tipo}, Función: {self._ImplanteMedico__funcion}, Material: {self.__material}, Tipo de fijación: {self.__tipo_fijacion}, Tamaño: {self.__tamaño}, Fecha de revisión: {self.__fecha_revision}, Fecha de mantenimiento: {self.__fecha_mantenimiento}"
 
 
 
 class ImplanteCadera(ImplanteMedico):
-    def __init__(self, tipo , funcion, material , tipo_fijacion, tamaño):
+    def __init__(self, tipo , funcion, material , tipo_fijacion, tamaño, fecha_revision, fecha_mantenimiento):
         super().__init__(tipo, funcion)
         self.__material = material
         self.__tipo_fijacion = tipo_fijacion
         self.__tamaño = tamaño
+        self.__fecha_revision = fecha_revision
+        self.__fecha_mantenimiento = fecha_mantenimiento
     def getMaterial(self):
         return self.__material
     def getTipoFijacion(self):
@@ -209,7 +219,7 @@ class ImplanteCadera(ImplanteMedico):
     def setTamaño(self, t):
         self.__tamaño = t
     def __str__(self):
-        return f"Tipo: {self._ImplanteMedico__tipo}, Función: {self._ImplanteMedico__funcion}, Material: {self.__material}, Tipo de fijación: {self.__tipo_fijacion}, Tamaño: {self.__tamaño}"
+        return f"Tipo: {self._ImplanteMedico__tipo}, Función: {self._ImplanteMedico__funcion}, Material: {self.__material}, Tipo de fijación: {self.__tipo_fijacion}, Tamaño: {self.__tamaño}, Fecha de revisión: {self.__fecha_revision}, Fecha de mantenimiento: {self.__fecha_mantenimiento}"
 
 
 
@@ -229,7 +239,9 @@ def menu_agregar_implante():
             electrodos = input('Ingrese el número de electrodos: ')
             conexion = input('Ingrese el tipo de conexión: ')
             frecuencia = input('Ingrese la frecuencia: ')
-            implante = MarcapasosCoronario( tipo, funcion, electrodos, conexion, frecuencia)
+            fecha_revision = input('Ingrese la fecha de revisión del implante (YYYY-MM-DD): ')
+            fecha_mantenimiento = input('Ingrese la fecha de mantenimiento del implante (YYYY-MM-DD): ')
+            implante = MarcapasosCoronario( tipo, funcion, electrodos, conexion, frecuencia, fecha_revision, fecha_mantenimiento)
             return implante
         elif tipo == 2:
             tipo = 'StentCoronario'
@@ -237,7 +249,9 @@ def menu_agregar_implante():
             longitud = input('Ingrese la longitud: ')
             diametro = input('Ingrese el diámetro: ')
             material = input('Ingrese el material: ')
-            implante = StentCoronario( tipo, funcion, longitud, diametro, material)
+            fecha_revision = input('Ingrese la fecha de revisión del implante (YYYY-MM-DD): ')
+            fecha_mantenimiento = input('Ingrese la fecha de mantenimiento del implante (YYYY-MM-DD): ')
+            implante = StentCoronario( tipo, funcion, longitud, diametro, material, fecha_revision, fecha_mantenimiento)
             return implante
         elif tipo == 3:
             tipo = 'ImplantesDentales '
@@ -245,7 +259,9 @@ def menu_agregar_implante():
             forma = input('Ingrese la forma: ')
             sistema_fijacion = input('Ingrese el sistema de fijación: ')
             material = input('Ingrese el material: ')
-            implante = ImplantesDentales( tipo, funcion, forma, sistema_fijacion, material)
+            fecha_revision = input('Ingrese la fecha de revisión del implante (YYYY-MM-DD): ')
+            fecha_mantenimiento = input('Ingrese la fecha de mantenimiento del implante (YYYY-MM-DD): ')
+            implante = ImplantesDentales( tipo, funcion, forma, sistema_fijacion, material, fecha_revision, fecha_mantenimiento)
             return implante
         elif tipo == 4:
             tipo = 'ImplanteRodilla'
@@ -253,7 +269,9 @@ def menu_agregar_implante():
             material = input('Ingrese el material: ')
             tipo_fijacion = input('Ingrese el tipo de fijación: ')
             tamaño = input('Ingrese el tamaño: ')
-            implante = ImplanteRodilla( tipo, funcion, material, tipo_fijacion, tamaño)
+            fecha_revision = input('Ingrese la fecha de revisión del implante (YYYY-MM-DD): ')
+            fecha_mantenimiento = input('Ingrese la fecha de mantenimiento del implante (YYYY-MM-DD): ')
+            implante = ImplanteRodilla( tipo, funcion, material, tipo_fijacion, tamaño, fecha_revision, fecha_mantenimiento)
             return implante
         elif tipo == 5:
             tipo = 'ImplanteCadera '
@@ -261,7 +279,10 @@ def menu_agregar_implante():
             material = input('Ingrese el material: ')
             tipo_fijacion = input('Ingrese el tipo de fijación: ')
             tamaño = input('Ingrese el tamaño: ')
-            implante = ImplanteCadera( tipo, funcion, material, tipo_fijacion, tamaño)
+            fecha_revision = input('Ingrese la fecha de revisión del implante (YYYY-MM-DD): ')
+            fecha_mantenimiento = input('Ingrese la fecha de mantenimiento del implante (YYYY-MM-DD): ')
+            implante = ImplanteCadera( tipo, funcion, material, tipo_fijacion, tamaño, fecha_revision, fecha_mantenimiento)
+            
             return implante
         else:
             print('Por favor seleccione un implante válido')
@@ -306,7 +327,7 @@ def main():
             tipo_implante = input('Ingrese el tipo de implante: ')
             funcion_implante = input('Ingrese la función del implante: ')
             implante = ImplanteMedico(tipo_implante, funcion_implante)
-            fecha_implantacion = input('Ingrese la fecha de implantación (dd/mm/aaaa): ')
+            fecha_implantacion = input('Ingrese la fecha de implantación (YYYY-MM-DD): ')
             medico_responsable = input('Ingrese el nombre del médico responsable: ')
             estado = input('Ingrese el estado del implante: ')
             paciente.asignar_implante(implante, fecha_implantacion, medico_responsable, estado)
@@ -351,7 +372,9 @@ def main():
                             electrodos = input('Ingrese el número de electrodos: ')
                             conexion = input('Ingrese el tipo de conexión: ')
                             frecuencia = input('Ingrese la frecuencia: ')
-                            implante = MarcapasosCoronario( tipo, funcion, electrodos, conexion, frecuencia)
+                            fecha_revision = input('Ingrese la fecha de revisión del implante (YYYY-MM-DD): ')
+                            fecha_mantenimiento = input('Ingrese la fecha de mantenimiento del implante (YYYY-MM-DD): ')
+                            implante = MarcapasosCoronario( tipo, funcion, electrodos, conexion, frecuencia, fecha_revision, fecha_mantenimiento)
                             print('Información del implante editada correctamente.')
                             return implante
                         elif nuevo_nombre== 2:
@@ -360,7 +383,9 @@ def main():
                             longitud = input('Ingrese la longitud: ')
                             diametro = input('Ingrese el diámetro: ')
                             material = input('Ingrese el material: ')
-                            implante = StentCoronario( tipo, funcion, longitud, diametro, material)
+                            fecha_revision = input('Ingrese la fecha de revisión del implante (YYYY-MM-DD): ')
+                            fecha_mantenimiento = input('Ingrese la fecha de mantenimiento del implante (YYYY-MM-DD): ')
+                            implante = StentCoronario( tipo, funcion, longitud, diametro, material, fecha_revision, fecha_mantenimiento)
                             print('Información del implante editada correctamente.')
                             return implante
                         elif nuevo_nombre == 3:
@@ -369,7 +394,9 @@ def main():
                             forma = input('Ingrese la forma: ')
                             sistema_fijacion = input('Ingrese el sistema de fijación: ')
                             material = input('Ingrese el material: ')
-                            implante = ImplantesDentales( tipo, funcion, forma, sistema_fijacion, material)
+                            fecha_revision = input('Ingrese la fecha de revisión del implante (YYYY-MM-DD): ')
+                            fecha_mantenimiento = input('Ingrese la fecha de mantenimiento del implante (YYYY-MM-DD): ')
+                            implante = ImplantesDentales( tipo, funcion, forma, sistema_fijacion, material, fecha_revision, fecha_mantenimiento)
                             print('Información del implante editada correctamente.')
                             return implante
                         elif nuevo_nombre== 4:
@@ -378,7 +405,9 @@ def main():
                             material = input('Ingrese el material: ')
                             tipo_fijacion = input('Ingrese el tipo de fijación: ')
                             tamaño = input('Ingrese el tamaño: ')
-                            implante = ImplanteRodilla( tipo, funcion, material, tipo_fijacion, tamaño)
+                            fecha_revision = input('Ingrese la fecha de revisión del implante (YYYY-MM-DD): ')
+                            fecha_mantenimiento = input('Ingrese la fecha de mantenimiento del implante (YYYY-MM-DD): ')
+                            implante = ImplanteRodilla( tipo, funcion, material, tipo_fijacion, tamaño, fecha_revision, fecha_mantenimiento)
                             print('Información del implante editada correctamente.')
                             return implante
                         elif nuevo_nombre == 5:
@@ -387,7 +416,9 @@ def main():
                             material = input('Ingrese el material: ')
                             tipo_fijacion = input('Ingrese el tipo de fijación: ')
                             tamaño = input('Ingrese el tamaño: ')
-                            implante = ImplanteCadera( tipo, funcion, material, tipo_fijacion, tamaño)
+                            fecha_revision = input('Ingrese la fecha de revisión del implante (YYYY-MM-DD): ')
+                            fecha_mantenimiento = input('Ingrese la fecha de mantenimiento del implante (YYYY-MM-DD): ')
+                            implante = ImplanteCadera( tipo, funcion, material, tipo_fijacion, tamaño, fecha_revision, fecha_mantenimiento)
                             print('Información del implante editada correctamente.')
                             return implante
                         else:
